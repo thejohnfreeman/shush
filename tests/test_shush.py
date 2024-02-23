8923551f43d4ac904939d99a5029c92e4dba6bd5
John Freeman <jfreeman08@gmail.com>
2024-02-22 18:37:12 -0600

    Make sh a test fixture

--- tests/test_shush.py
+++ tests/test_shush.py
@@ -21,71 +21,73 @@ def named_temp_file():
         file.close()
         os.remove(path)
 
-sh = shush.Shell().here
+@pytest.fixture()
+def sh():
+    return shush.Shell().here
 
-def test_success():
+def test_success(sh):
     sh.true > sh
 
-def test_failure():
+def test_failure(sh):
     with pytest.raises(subprocess.CalledProcessError):
         sh.false > sh
 
-def test_output():
+def test_output(sh):
     proc = sh> sh.echo('hello')
     assert(proc.stdout == b'hello\n')
 
-def test_prompt():
+def test_prompt(sh):
     sh> sh.true
 
-def test_pipe():
+def test_pipe(sh):
     proc = sh> sh.echo('hello') | sh.cat
     assert(proc.stdout == b'hello\n')
 
-def test_tail():
+def test_tail(sh):
     proc = sh> sh.echo('hello\ngoodbye') | sh.tail('-1')
     assert(proc.stdout == b'goodbye\n')
 
-def test_arg_if_then():
+def test_arg_if_then(sh):
     proc = sh> sh.echo('hello', False and 'goodbye')
     assert(proc.stdout == b'hello\n')
 
-def test_arg_none():
+def test_arg_none(sh):
     proc = sh> sh.echo(None, 'hello')
     assert(proc.stdout == b'hello\n')
 
-def test_arg_if_else():
+def test_arg_if_else(sh):
     proc = sh> sh.echo('goodbye' if False else 'hello')
     assert(proc.stdout == b'hello\n')
 
-def test_arg_list():
+def test_arg_list(sh):
     proc = sh> sh.echo('a', ['b', 'c'], 'd')
     assert(proc.stdout == b'a b c d\n')
 
-def test_arg_tuple():
+def test_arg_tuple(sh):
     proc = sh> sh.echo('a', ('b', 'c'), 'd')
     assert(proc.stdout == b'a b c d\n')
 
-def test_kwarg_bool():
+def test_kwarg_bool(sh):
     proc = sh> sh.python(version=True)
     assert(proc.stdout.startswith(b'Python 3.'))
 
-def test_kwarg_short():
+def test_kwarg_short(sh):
     proc = sh> sh.python(c='import sys; sys.stdout.write("hello")')
     assert(proc.stdout == b'hello')
 
-def test_kwarg_long():
+def test_kwarg_long(sh):
     proc = sh> sh.env.env({'foo': 'bar'}) | sh.grep(regexp='foo')
     assert(proc.stdout == b'foo=bar\n')
 
-def test_stdin_str():
+def test_stdin_str(sh):
     proc = sh> (sh.cat < 'hello')
     assert(proc.stdout == b'hello')
 
-def test_stdin_bytes():
+def test_stdin_bytes(sh):
     proc = sh> (sh.cat < b'hello')
     assert(proc.stdout == b'hello')
 
-def test_stdin_file():
+def test_stdin_file(sh):
     with named_temp_file() as (file, path):
         file.write(b'hello')
         file.close()
@@ -94,59 +96,59 @@ def test_stdin_file():
         assert(proc.stdout == b'hello')
         assert(file.closed)
 
-def test_stdin_path():
+def test_stdin_path(sh):
     with named_temp_file() as (file, path):
         file.write(b'hello')
         file.close()
         proc = sh> (sh.cat < path)
         assert(proc.stdout == b'hello')
 
-def test_shell_env():
-    sh_ = sh(env={'foo': 'bar'})
-    proc = sh_> sh_.env | sh_.grep('foo')
+def test_shell_env(sh):
+    sh = sh(env={'foo': 'bar'})
+    proc = sh> sh.env | sh.grep('foo')
     assert(proc.stdout == b'foo=bar\n')
 
-def test_shell_env_op():
-    sh_ = sh % {'foo': 'bar'}
-    proc = sh_> sh_.env | sh_.grep('foo')
+def test_shell_env_op(sh):
+    sh = sh % {'foo': 'bar'}
+    proc = sh> sh.env | sh.grep('foo')
     assert(proc.stdout == b'foo=bar\n')
 
-def test_shell_cwd():
-    cwd = pathlib.Path('.').resolve().parent
-    sh_ = sh(cwd=cwd)
-    proc = sh_> sh_.pwd
+def test_shell_cwd(sh):
+    cwd = pathlib.Path('.').resolve()
+    sh = sh(cwd=cwd)
+    proc = sh> sh.pwd
     assert(proc.stdout == f'{cwd}\n'.encode())
 
-def test_shell_cwd_op():
-    cwd = pathlib.Path('.').resolve().parent
-    sh_ = sh @ cwd
-    proc = sh_> sh_.pwd
+def test_shell_cwd_op(sh):
+    cwd = pathlib.Path('.').resolve()
+    sh = sh @ cwd
+    proc = sh> sh.pwd
     assert(proc.stdout == f'{cwd}\n'.encode())
 
-def test_command_env():
+def test_command_env(sh):
     proc = sh> sh.env.env({'foo': 'bar'}) | sh.grep('foo')
     assert(proc.stdout == b'foo=bar\n')
 
-def test_command_env_op():
+def test_command_env_op(sh):
     proc = sh> sh.env % {'foo': 'bar'} | sh.grep('foo')
     assert(proc.stdout == b'foo=bar\n')
 
-def test_command_cwd():
+def test_command_cwd(sh):
     cwd = pathlib.Path('.').resolve().parent
     proc = sh> sh.pwd.cwd(cwd)
     assert(proc.stdout == f'{cwd}\n'.encode())
 
-def test_command_cwd_op():
+def test_command_cwd_op(sh):
     cwd = pathlib.Path('.').resolve().parent
     proc = sh> sh.pwd @ cwd
     assert(proc.stdout == f'{cwd}\n'.encode())
 
-def test_command_cwd_chain():
+def test_command_cwd_chain(sh):
     cwd = pathlib.Path('.').resolve().parent
     proc = sh> sh.pwd.cwd('/does/not/exist').cwd(cwd)
     assert(proc.stdout == f'{cwd}\n'.encode())
 
-def test_stdout_file():
+def test_stdout_file(sh):
     with named_temp_file() as (file, path):
         sh.echo('hello') > file
         # Input files should be closed.
@@ -154,32 +156,32 @@ def test_stdout_file():
         assert(not file.closed)
         assert(path.read_bytes() == b'hello\n')
 
-def test_stdout_path():
+def test_stdout_path(sh):
     with named_temp_file() as (file, path):
         file.close()
         sh.echo('hello') > path
         assert(path.read_bytes() == b'hello\n')
 
-def test_stdout_str():
+def test_stdout_str(sh):
     with named_temp_file() as (file, path):
         file.close()
         sh.echo('hello') > str(path)
         assert(path.read_bytes() == b'hello\n')
 
-def test_stdout_bytes():
+def test_stdout_bytes(sh):
     with named_temp_file() as (file, path):
         file.close()
         sh.echo('hello') > bytes(path)
         assert(path.read_bytes() == b'hello\n')
 
-def test_stdout_nowhere():
+def test_stdout_nowhere(sh):
     proc = sh.echo('hello') > sh.nowhere
     assert(proc.stdout is None)
 
-def test_join():
+def test_join(sh):
     proc = sh> sh.python(c='import sys; sys.stderr.write("hello")').join()
     assert(proc.stdout == b'hello')
 
-def test_command_path():
+def test_command_path(sh):
     proc = sh> sh['/bin/echo']('hello')
     assert(proc.stdout == b'hello\n')
